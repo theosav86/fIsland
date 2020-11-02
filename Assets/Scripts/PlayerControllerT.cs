25acdf832342d5b3c5f3a55692effbc86ecd5c1d
theosav <savvaidis_teo@onskull.com>
2020-11-03 00:23:45 +0200

    Player Controller

---
+++ Assets/Scripts/PlayerControllerT.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerT : MonoBehaviour
+{
+    #region Variables
+
+    private float xAxis, zAxis;
+
+    public float moveSpeed = 5f;
+
+    public float jumpForce = 5f;
+
+    public bool isJumping = false;
+
+    private float jumpDelayTimer = 1f;
+
+    private Rigidbody playerBody;
+
+    private float mouseX;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal"); // A & D
+        zAxis = Input.GetAxis("Vertical");  // W & S
+
+        mouseX = Input.mousePosition.x;
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            Jump();
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            RotatePlayer();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movementVector = new Vector3(xAxis, 0f, zAxis);
+
+        movementVector.Normalize();
+
+        playerBody.position += movementVector * moveSpeed * Time.fixedDeltaTime;        
+    }
+
+    private void Jump()
+    {
+        isJumping = true;
+
+        playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        Invoke(nameof(JumpReady), jumpDelayTimer);
+    }
+
+    private void JumpReady()
+    {
+        isJumping = false;
+    }
+
+    private void RotatePlayer()
+    {
+        transform.localRotation = Quaternion.Euler(0f, mouseX, 0f);
+    }
+}
