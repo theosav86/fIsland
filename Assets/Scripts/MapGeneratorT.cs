e1be86b5d2a0616749d63bf9660ffba00d71606f
theosav <savvaidis_teo@onskull.com>
2020-11-16 00:55:56 +0200

    Randomized board fixed

--- Assets/Scripts/MapGeneratorT.cs
+++ Assets/Scripts/MapGeneratorT.cs
@@ -20,26 +20,19 @@ public class MapGeneratorT : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(currentLayout == LayoutNames.CROSS)
+        List<GameObject> randomBoard = new List<GameObject>(RandomizeBoard(tiles));
+
+        Debug.Log(randomBoard.Count);
+
+        if (currentLayout == LayoutNames.CROSS)
         {
-            for(int i = 0; i < crossLayout.Length; i++)
+            for (int i = 0; i < crossLayout.Length; i++)
             {
-                int tileToSpawn = Random.Range(0, tiles.Count);
-
-                if(tiles[tileToSpawn] != null)
-                {
-                    Instantiate(tiles[tileToSpawn], crossLayout[i].position, Quaternion.identity);
-                    tiles.RemoveAt(tileToSpawn);
-                }
-                else
-                {
-                    i -= 1;
-                }
-                
-            }
-
+                Instantiate(randomBoard[i], crossLayout[i].position, Quaternion.identity);
+            }            
+            
             Debug.Log("Cross Layout Set Up");
-        }
+        }        
     }
 
     // Update is called once per frame
@@ -47,4 +40,32 @@ public class MapGeneratorT : MonoBehaviour
     {
         
     }
+
+    //Takes a List<GameObject> and returns it with randomized elements
+    private List<GameObject> RandomizeBoard(List<GameObject> tileList)
+    {
+        //Create a temporary list so we do not mess the original one
+        List<GameObject> tempTileList = new List<GameObject>(tileList);
+
+        //This is the List to return
+        List<GameObject> outputList = new List<GameObject>();
+
+        //While tempList has elements in it
+        while(tempTileList.Count > 0)
+        {
+            //Select a random number between 0 and tempList size (size being reduced each iteration due to RemoveAt(index);)
+            int index = Random.Range(0, tempTileList.Count);
+
+            //Add the random element to the output list
+            outputList.Add(tempTileList[index]);
+
+            //remove that element from the tempList
+            tempTileList.RemoveAt(index);
+            
+        }
+
+        //Return the randomized list
+        return outputList;
+    }
+    
 }
